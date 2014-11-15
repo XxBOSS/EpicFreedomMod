e3762f2c560f308293c3a37a692e07ee49aee46b
Tyler Wrenn <mcbeat257@gmail.com>
2014-11-15 13:48:58 -0500

    Added /kiss

---
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_kiss.java
@@ -0,0 +1,33 @@
+package me.StevenLawson.TotalFreedomMod.Commands;
+
+import me.StevenLawson.TotalFreedomMod.Config.TFM_ConfigEntry;
+import me.StevenLawson.TotalFreedomMod.TFM_AdminList;
+import me.StevenLawson.TotalFreedomMod.TFM_Util;
+import me.StevenLawson.TotalFreedomMod.TotalFreedomMod;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+@CommandPermissions(level = AdminLevel.SUPER, source = SourceType.BOTH, blockHostConsole = true)
+@CommandParameters(description = "Close server to non-superadmins.", usage = "/<command> [on | off]", aliases = "am")
+public class Command_kiss extends TFM_Command
+{
+    @Override
+    public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
+    {
+        if (args.length != 1)
+        {
+            return false;
+        }
+
+        final Player player = getPlayer(args[0]);
+
+        if (player == null)
+        {
+            sender.sendMessage(TotalFreedomMod.PLAYER_NOT_FOUND);
+            return true;
+        }
+    TFM_Util.adminAction(sender.getName(), "Kissing " + player.getName(), true);
+        return true;
+}
+}
