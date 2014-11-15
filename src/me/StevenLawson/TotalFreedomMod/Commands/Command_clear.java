20fe981d6035da2415f1b48a89cc58498bf3805a
Tyler Wrenn <mcbeat257@gmail.com>
2014-11-15 17:03:28 -0500

    All fixed? YES

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_clear.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_clear.java
@@ -1,6 +1,9 @@
 package me.StevenLawson.TotalFreedomMod.Commands;
 
+import me.StevenLawson.TotalFreedomMod.TFM_Util;
 import me.StevenLawson.TotalFreedomMod.TotalFreedomMod;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
     import org.bukkit.command.Command;
     import org.bukkit.command.CommandSender;
     import org.bukkit.entity.Player;
@@ -15,10 +18,10 @@ public class Command_clear extends TFM_Command
     @Override
     public boolean run(final CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
-            final Player player = getPlayer(args[1]);
+            Player player = (Player)sender;
             player.closeInventory();
             player.getInventory().clear();
-            player.sendMessage("Your inventory has been cleared.");
+            player.sendMessage(ChatColor.GOLD + "Your inventory has been cleared.");
             return true;
     }
 }
