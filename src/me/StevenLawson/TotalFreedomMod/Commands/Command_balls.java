17c85b86cc5a2388d289df0af1aebd12a8514727
Tyler Wrenn <mcbeat257@gmail.com>
2014-11-16 15:09:31 -0500

    Updated

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_balls.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_balls.java
@@ -1,16 +1,19 @@
 package me.StevenLawson.TotalFreedomMod.Commands;
 
+import me.StevenLawson.TotalFreedomMod.TFM_GameRuleHandler;
+import me.StevenLawson.TotalFreedomMod.TFM_PlayerData;
 import me.StevenLawson.TotalFreedomMod.TFM_Util;
 import me.StevenLawson.TotalFreedomMod.TotalFreedomMod;
-import me.confuser.barapi.BarAPI;
 import net.minecraft.util.org.apache.commons.lang3.ArrayUtils;
 import net.minecraft.util.org.apache.commons.lang3.StringUtils;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
 
-@CommandPermissions(level = AdminLevel.SUPER, source = SourceType.BOTH)
+@CommandPermissions(level = AdminLevel.SENIOR, source = SourceType.BOTH)
 @CommandParameters(
         description = "Make someone's balls explode.",
         usage = "/<command>")
@@ -31,12 +34,22 @@ public class Command_balls extends TFM_Command
             sender.sendMessage(TotalFreedomMod.PLAYER_NOT_FOUND);
             return true;
         }
-
         TFM_Util.bcastMsg(player.getName() + "'s balls exploded.", ChatColor.RED);
         player.sendMessage("This is for no reason at all. We just like to blow peoples balls up.");
         player.getWorld().createExplosion(player.getLocation(), 4F);
+        player.getWorld().strikeLightning(player.getLocation());
+        new BukkitRunnable()
+        {
+       @Override
+       public void run()
+       {
         player.getWorld().createExplosion(player.getLocation(), 4F);
+        player.getWorld().strikeLightning(player.getLocation());
+        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "gamerule keepInventory true");
         player.setHealth(0.0);
-        return true;
+        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "gamerule keepInventory false");
+       }
+       }.runTaskLater(plugin, 1L * 10L);
+      return true;
     }
 }
