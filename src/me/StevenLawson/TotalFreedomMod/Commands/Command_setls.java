5991ae5ab3ab57b5754220237f615fca3a07f279
Tyler Wrenn <mcbeat257@gmail.com>
2014-12-31 14:57:25 -0500

    Makes silent set world edit modification

---
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_setls.java
@@ -0,0 +1,23 @@
+package me.StevenLawson.TotalFreedomMod.Commands;
+
+import me.StevenLawson.TotalFreedomMod.Bridge.TFM_WorldEditBridge;
+import me.StevenLawson.TotalFreedomMod.TFM_Util;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+@CommandPermissions(level = AdminLevel.ALL, source = SourceType.ONLY_CONSOLE)
+@CommandParameters(description = "Sets everyone's Worldedit block modification limit to a custom amount (defaults to 500).", usage = "/<command> [limit]")
+public class Command_setls extends TFM_Command
+{
+    @Override
+    public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
+    {
+        int limit = (args.length >= 1 ? Integer.parseInt(args[0]) : 1000);
+        for (final Player player : server.getOnlinePlayers())
+        {
+            TFM_WorldEditBridge.setLimit(player, limit);
+        }
+        return true;
+    }
+}
